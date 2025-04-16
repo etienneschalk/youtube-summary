e30a45b104ab8966de3dcba6ca187c0f8c5b24fa
eschalk <etienne.schalk@gmail.com>
2025-04-16 10:36:03 +0200

    Improve scrapper: scrap description with JSON mode and try system for regex

--- ai_xp/scrapper.py
+++ ai_xp/scrapper.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
+import json
 from pathlib import Path
 import re
+from typing import Literal, Self
 from bs4 import BeautifulSoup
 
 
 @dataclass(frozen=True, kw_only=True)
 class YouTubeHtmlScrapper:
     soup: BeautifulSoup
+    # Regex courtesy of
+    # https://stackoverflow.com/questions/1060616/how-can-regex-ignore-escaped-quotes-when-matching-strings
     string_extraction_pattern = r'"((?:\\\\|\\"|[^"])*+)"'
 
     @classmethod
@@ -14,20 +18,43 @@ class YouTubeHtmlScrapper:
         soup = BeautifulSoup(path.read_text(), features="html.parser")
         return cls(soup=soup)
 
+    @classmethod
+    def from_url(cls, url: str) -> Self | None:
+        import requests
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an error for bad status codes
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to fetch URL: {e}")
+            return None
+
+        soup = BeautifulSoup(response.text, features="html.parser")
+        return cls(soup=soup)
+
     def title(self) -> str:
         return str(self.soup.title.text).rstrip(" - YouTube")
 
-    def short_description(self) -> str:
+    def short_description(self, *, mode: Literal["json", "regex"] = "json") -> str:
         script_text = self.soup.body.script.text.replace("\n", "")
 
-        # Regex courtesy of
-        # https://stackoverflow.com/questions/1060616/how-can-regex-ignore-escaped-quotes-when-matching-strings
-        shortDescription = (
-            re.findall(
-                r"shortDescription:\s+" + self.string_extraction_pattern, script_text
-            )[0]
-            .replace(r"\"", '"')
-            .replace("\\n", "\n")
-        )
+        if mode == "regex":
+            return self.try_to_extract_short_description(script_text)
+        elif mode == "json":
+            obj = json.loads(script_text.lstrip("var ytInitialPlayerResponse = ")[:-1])
+            return obj["videoDetails"]["shortDescription"]
 
-        return shortDescription
+        raise NotImplementedError
+
+    def try_to_extract_short_description(self, script_text: str) -> str:
+        patterns = [
+            r'shortDescription":' + self.string_extraction_pattern,
+            r"shortDescription:\s*" + self.string_extraction_pattern,
+        ]
+        for pattern in patterns:
+            try:
+                res = re.findall(pattern, script_text)[0]
+                return res.replace("\\n", "\n")
+            except IndexError:
+                pass
+        return ""
